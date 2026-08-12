988822d37a3a9ad23a589910a6c1858be9c63bdb
ayanrajput5264 <ayanait87@gmail.com>
2026-08-12 19:01:44 +0500

    nabar done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar1 from './component/Navbar1'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar1 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Navbar1.jsx
@@ -0,0 +1,46 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import image from '../assets/logo.png'
+import { Routes,Route,Link,NavLink } from 'react-router-dom';
+import "../App.css"
+
+function Navbar1() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid className='space'>
+        <Navbar.Brand href="#"><img src={image} alt="" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Pages</Nav.Link>
+           
+            <Nav.Link href="#" disable>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  );
+}
+
+export default Navbar1;
