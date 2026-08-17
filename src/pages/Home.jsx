1598f774dba3b5ecd8ec1b686b72c802eece5b7b
ayanrajput5264 <ayanait87@gmail.com>
2026-08-17 18:47:10 +0500

    Hero section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../App.css"
+import heroImage from "../assets/hero.png";
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>helo </h1>
+    <div className="main">
+      <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center roundged-3 border shadowl-">
+        {" "}
+        <div className="col-lg-7 p-3 p-lg-5 pt-lg-3 g-10">
+          {" "}
+          <h1 className="display-3 lh-1 text-body-emphasis">
+            Best furniture for Your castle
+          </h1>{" "}
+          <h1 className="lead display-4 fw-bold lh-1 text-body-emphasis">
+           New Furniture Collection <br />Trend in 2026
+          </h1>{" "}
+          <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+            {" "}
+            <button
+              type="button"
+              className="btn btn-lg px-4 me-md-2 fw-bold button"
+            >
+              Shop Now
+            </button>{" "}
+            
+          </div>{" "}
+        </div>{" "}
+        <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          {" "}
+          <img
+            className="rounded-lg-3"
+            src={heroImage}
+            alt=""
+            width="720"
+          />{" "}
+        </div>{" "}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
