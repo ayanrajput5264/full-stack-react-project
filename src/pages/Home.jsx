ddc914d6ea3bfcb8a105d8cbb850f924bb614415
ayanrajput5264 <ayanait87@gmail.com>
2026-08-19 09:16:37 +0500

    featured section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@ import heroImage from "../assets/hero.png";
 
 const Home = () => {
   return (
-    <div className="main">
-      <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center roundged-3 border shadowl-">
+    <div>
+      <div className=" main row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center roundged-3 border shadowl-">
         {" "}
-        <div className="col-lg-7 p-3 p-lg-5 pt-lg-3 g-10">
+        <div className=" container col-lg-7 p-3 p-lg-5 pt-lg-3 g-10">
           {" "}
-          <h1 className="display-3 lh-1 text-body-emphasis">
+          <h1 className=" h1 display-3 lh-1 ">
             Best furniture for Your castle
           </h1>{" "}
           <h1 className="lead display-4 fw-bold lh-1 text-body-emphasis">
@@ -27,16 +27,20 @@ const Home = () => {
             
           </div>{" "}
         </div>{" "}
-        <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+
           {" "}
           <img
-            className="rounded-lg-3"
+            className=" img rounded-lg-3"
             src={heroImage}
             alt=""
             width="720"
           />{" "}
-        </div>{" "}
+        
       </div>
+
+      {/* feturde product  */}
+
+      
     </div>
   );
 };
